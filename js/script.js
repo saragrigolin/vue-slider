e12a3774283b73c0246a14efeb8dec28a49b2aa0
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-16 13:46:15 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+Descrizione:
+Partendo dal markup allegato, rifare lo slider ma questa volta usando Vue.
+Bonus:
+1- al click su uno dei pallini mostrare l’immagine in posizione corrispondente
+2- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
+Consigli del giorno:
+Leggete sempre la documentazione, partite dalle cose basilari per poi aggiungere complessità.
+Ricordiamo sempre che è necessario capire bene prima che dati dobbiamo andare a manipolare.
+*/
+
